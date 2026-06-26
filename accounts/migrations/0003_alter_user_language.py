b7248b6539564a34abf179e513f09a6a47243f0d
Mohammed Awal Bawah Issah <mohammedawalbawahissah@gmail.com>
2026-06-26 11:12:39 +0000

    Add: Notifications setting update

--- accounts/migrations/0003_alter_user_language.py
+++ accounts/migrations/0003_alter_user_language.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_otpverification_alter_user_role'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
